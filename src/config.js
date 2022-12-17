474f7b104a3d17230916488bf444d7f890121659
josnnydev <pjosnny@gmail.com>
2022-12-16 21:50:28 -0400

    add

--- src/config.js
+++ src/config.js
@@ -4,6 +4,7 @@ const COINBASE_API_KEY = process.env.COINBASE_API_KEY;
 const COINBASE_WEBHOOK_SECRET = process.env.COINBASE_WEBHOOK_SECRET;
 const DOMAIN = process.env.DOMAIN || 'http://localhost:3001';
 
+
 module.exports ={
   COINBASE_API_KEY,
   COINBASE_WEBHOOK_SECRET,
