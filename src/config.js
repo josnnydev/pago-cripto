c5bf4746cfbb181b78b78f8b5379bc2544eb15e8
josnnydev <pjosnny@gmail.com>
2023-01-20 14:24:09 -0400

    update

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ require ('dotenv/config')
 
 const COINBASE_API_KEY = process.env.COINBASE_API_KEY;
 const COINBASE_WEBHOOK_SECRET = process.env.COINBASE_WEBHOOK_SECRET;
-const DOMAIN = process.env.DOMAIN || 'http://localhost:3001';
+const DOMAIN = process.env.DOMAIN || 'http://localhost:3000';
 
 
 module.exports ={
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const path = require('path')
 
 const app = express();
-const port = process.env.PORT || 3001
+const port = process.env.PORT || 3000
 const {
   COINBASE_API_KEY,
   COINBASE_WEBHOOK_SECRET,
@@ -14,12 +14,12 @@ const {
 const { Client, resources, Webhook } = require("coinbase-commerce-node");
 
 app.use(morgan('dev'))
-app.use(express.json({
-  verify: (req,res,buf)=>{
-    req.rawBody = buf
-  }
-}))
-app.use(express.static(path.join(__dirname, "public")));
+  app.use(express.json({
+    verify: (req,res,buf)=>{
+      req.rawBody = buf
+    }
+  }))
+ app.use(express.static(path.join(__dirname, "public")));
 
 Client.init(COINBASE_API_KEY);
 const { Charge } = resources;
@@ -37,48 +37,48 @@ app.get("/create-charge", async (req, res) => {
       customer_id: 'id_1',
       customer_name: 'josnny'
     },
-    redirect_url: `${DOMAIN}success-payment`,
-    cancel_url: `${DOMAIN}cancel-payment`
+    redirect_url: `${DOMAIN}/success-payment`,
+    cancel_url: `${DOMAIN}/cancel-payment`
   };
  const charge = await Charge.create(chargeData);
  res.send(charge)
 
 });
 
-  app.post('/payment-handler',(req,res)=>{
-  const rawBody = req.rawBody
-  const signature = req.headers["x-cc-webhook-signature"];
-  const webhookSecret = COINBASE_WEBHOOK_SECRET;
+   app.post('/payment-handler',(req,res)=>{
+   const rawBody = req.rawBody
+   const signature = req.headers["x-cc-webhook-signature"];
+   const webhookSecret = COINBASE_WEBHOOK_SECRET;
 
 
-  let event
-  try {
-   event = Webhook.verifyEventBody(rawBody,signature,webhookSecret)
+   let event
+   try {
+    event = Webhook.verifyEventBody(rawBody,signature,webhookSecret)
 
-   if(event.type === 'charge:pending'){
-    console.log('pago pendiente')
+    if(event.type === 'charge:pending'){
+     console.log('pago pendiente')
+    }
+    if(event.type === 'charge:confirmed'){
+     console.log('pago confirmado')
+    }
+    if(event.type === 'charge:failed'){
+     console.log('pago fallido')
+    }
+
+    return res.send(event.id)
+   } catch (error) {
+     console.log(error)
+     res.status(400).send('fail')
    }
-   if(event.type === 'charge:confirmed'){
-    console.log('pago confirmado')
-   }
-   if(event.type === 'charge:failed'){
-    console.log('pago fallido')
-   }
+   })
 
-   return res.send(event.id)
-  } catch (error) {
-    console.log(error)
-    res.status(400).send('fail')
-  }
+  app.get('/success-payment',(req,res)=>{
+    res.send('payment completed')
   })
 
-app.get('/success-payment',(req,res)=>{
-  res.send('payment completed')
-})
-
-app.get('/cancel-payment',(req,res)=>{
-  res.send('payment canceled')
-})
+  app.get('/cancel-payment',(req,res)=>{
+    res.send('payment canceled')
+  })
 
 
 
